b0278c469df0c159272904ccaae6b1f7e5689842
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-20 16:50:51 -0300

    exercicios-switch-e-case

---
+++ fundamentos/4.1/switch-e-case/scripts.js
@@ -0,0 +1,15 @@
+let estado = 'aprovada';
+
+switch (estado) {
+    case 'aprovada':
+        console.log('Parabéns, você foi aprovada(o)!');
+        break;
+
+    case 'lista':
+        console.log('Você está na lista de espera.');
+        break;
+
+    case 'reprovada':
+        console.log('Você foi reprovada(o).');
+        break;
+}
