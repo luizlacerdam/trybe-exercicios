d80537e81b1ab6dd6206ece8426702e4bac273f0
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-22 17:36:23 -0300

    exercicio-2-tamara

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicio-tamara.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicio-tamara.js
@@ -1,7 +1,22 @@
-let sum = 0;
+//Exercicio 1
 
-for (let i = 0; i <= 50; i += 1) {
-    sum += i;
+// let sum = 0;
+
+// for (let i = 0; i <= 50; i += 1) {
+//     sum += i;
+// }
+
+// console.log(sum);
+
+//Exercicio 2 intevalo 2 - 150 sao divisiveis por 3
+let count = 0;
+
+for (let i = 2; i <= 150; i += 1) {
+    if (i % 3 == 0) {
+        count += 1;
+    }
 }
-
-console.log(sum);
+if (count == 50) {
+console.log('Mensagem secreta');;
+}
+console.log(count)
