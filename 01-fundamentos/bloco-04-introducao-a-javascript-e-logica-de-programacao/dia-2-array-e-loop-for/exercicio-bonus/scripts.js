0279a526c2d9fb12f37b3e7950ece83b3ab06e11
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-22 10:53:59 -0300

    exercicio-bonus-2

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicio-bonus/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicio-bonus/scripts.js
@@ -14,3 +14,16 @@ for (let index = 1; index < array.length; index += 1) {
 
   console.log(array);
 
+//Exercicio 2
+
+for (let index = 1; index < array.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (array[index] > array[secondIndex]) {
+        let position = array[index];
+        array[index] = array[secondIndex];
+        array[secondIndex] = position;
+      }
+    }
+  }
+
+  console.log(array);
