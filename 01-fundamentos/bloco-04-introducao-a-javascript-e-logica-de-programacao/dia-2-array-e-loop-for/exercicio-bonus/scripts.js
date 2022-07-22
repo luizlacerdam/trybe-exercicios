63a4e13f1b8edf0109b845df6ab379176a453a4d
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-22 14:53:38 -0300

    finalizacao-exercicio-bonus

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicio-bonus/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicio-bonus/scripts.js
@@ -2,28 +2,45 @@ let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //Exercicio 1
 
-for (let index = 1; index < array.length; index += 1) {
-    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-      if (array[index] < array[secondIndex]) {
-        let position = array[index];
-        array[index] = array[secondIndex];
-        array[secondIndex] = position;
-      }
-    }
-  }
+// for (let index = 1; index < array.length; index += 1) {
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//       if (array[index] < array[secondIndex]) {
+//         let position = array[index];
+//         array[index] = array[secondIndex];
+//         array[secondIndex] = position;
+//       }
+//     }
+// }
 
-  console.log(array);
+// console.log(array);
 
 //Exercicio 2
 
-for (let index = 1; index < array.length; index += 1) {
-    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-      if (array[index] > array[secondIndex]) {
-        let position = array[index];
-        array[index] = array[secondIndex];
-        array[secondIndex] = position;
-      }
-    }
-  }
+// for (let index = 1; index < array.length; index += 1) {
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//       if (array[index] > array[secondIndex]) {
+//         let position = array[index];
+//         array[index] = array[secondIndex];
+//         array[secondIndex] = position;
+//       }
+//     }
+// }
 
-  console.log(array);
+// console.log(array);
+
+//Exercicio 3
+let mult;
+let matrix = []
+for (let index = 0; index < array.length; index += 1) {
+    
+    if (index < array.length) {
+        mult = array[index] * 2;
+        matrix.push(mult)
+    } else {
+        mult = array[index] * array[index + 1];
+        matrix.push(mult)
+    }
+        
+}
+
+console.log(matrix);
