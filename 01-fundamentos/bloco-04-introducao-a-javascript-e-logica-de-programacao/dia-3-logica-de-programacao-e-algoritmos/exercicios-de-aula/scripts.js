da847c88acb6754e89a5bd7c38dca552653c6399
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-24 10:56:41 -0300

    exercicio-3-completo

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicios-de-aula/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicios-de-aula/scripts.js
@@ -16,3 +16,38 @@ for (let i = 0; i < palavra.length; i += 1) {
     inverso.unshift(palavra[i])
 }
 console.log(inverso);
+
+//Exercicio 3.1
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let palavraMaior;
+
+for (let i = 0; i < array.length; i += 1) {
+    if (i == 0) {
+        palavraMaior = array[i];
+    } else {
+        if (array[i].length > palavraMaior.length) {
+            palavraMaior = array[i];
+        }
+    }
+}
+
+console.log(palavraMaior);
+
+//Execicio 3.2
+
+let palavraMenor;
+
+for (let i = 0; i < array.length; i += 1) {
+    if (i == 0) {
+        palavraMenor = array[i];
+    } else {
+        if (array[i].length < palavraMenor.length) {
+            palavraMenor = array[i];
+        }
+    }
+}
+
+console.log(palavraMenor);
+
+
