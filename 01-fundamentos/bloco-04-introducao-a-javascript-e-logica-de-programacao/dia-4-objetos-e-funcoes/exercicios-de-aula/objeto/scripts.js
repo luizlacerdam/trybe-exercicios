6a56210bf20bab4d8c2b844d5079f6632684e057
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-25 16:34:51 -0300

    exercicio-1-e-2

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercicios-de-aula/objeto/scripts.js
@@ -0,0 +1,7 @@
+//Exercicio 1 e 2
+
+let player = {name: 'Marta', lastName: 'Silva', age: 34};
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' de idade.' );
+
+//Exercicio 3
