edc4b6fb8fba0570b0f38b333fb0f6de7a74a27f
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-25 18:52:29 -0300

    exercicio-11

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercicios-de-aula/functions/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercicios-de-aula/functions/scripts.js
@@ -150,20 +150,76 @@ const c = 300;
 // console.log(umImpar(a,b,c));
 
 //Exercicio 10
-const valorC = 100;
-const valorV = 200;
-let impostoA = 1.2;
+// const valorC = 100;
+// const valorV = 200;
+// let impostoA = 1.2;
 
-function custoDoProduto(valorCusto,valorVenda,imposto) {
+// function custoDoProduto(valorCusto,valorVenda,imposto) {
 
-if (valorCusto < 0 || valorVenda < 0) {
-    return 'Custo ou venda negativos.';
+// if (valorCusto < 0 || valorVenda < 0) {
+//     return 'Custo ou venda negativos.';
+// }
+
+// let valorCustoTotal = valorCusto * imposto;
+// let lucro = valorVenda - valorCustoTotal;
+
+// return lucro;
+// }
+
+// console.log(custoDoProduto(valorC,valorV, impostoA));
+
+//Exercicio 11
+let salario = 3000.00;
+
+function calculoSalario(salarioBruto) {
+let inssAliquota;
+let inssMaxAliquota;
+let irAliquota;
+let irParcela;
+
+
+//INSS
+if (salarioBruto <= 1556.94) {
+    inssAliquota = 8;
+    inssMaxAliquota = 0;
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+    inssAliquota = 9;
+    inssMaxAliquota = 0;
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    inssAliquota = 11;
+    inssMaxAliquota = 0;
+} else if (salarioBruto > 5189.82) {
+    inssAliquota = 0;
+    inssMaxAliquota = 570.88;
 }
 
-let valorCustoTotal = valorCusto * imposto;
-let lucro = valorVenda - valorCustoTotal;
+let salarioInssDeduzido = (salarioBruto * ((100 - inssAliquota)/100)) - inssMaxAliquota;
 
-return lucro;
+// console.log(salarioInssDeduzido);
+
+//IR
+if (salarioInssDeduzido <= 1903.98) {
+    irAliquota = 0;
+    irParcela = 0;
+} else if (salarioInssDeduzido >= 1903.99 && salarioInssDeduzido <= 2826.65) {
+    irAliquota = 7.5;
+    irParcela = 142.8;
+} else if (salarioInssDeduzido >= 2826.66 && salarioInssDeduzido <= 3751.05) {
+    irAliquota = 15;
+    irParcela = 354.8;
+} else if (salarioInssDeduzido >= 3751.06 && salarioInssDeduzido <= 4664.68) {
+    irAliquota = 22.5;
+    irParcela = 636.13;
+} else if (salarioInssDeduzido > 4664.68) {
+    irAliquota = 27.5;
+    irParcela = 869.36;
 }
 
-console.log(custoDoProduto(valorC,valorV, impostoA));
+let valorIr = (salarioInssDeduzido * (irAliquota / 100)) - irParcela;
+
+let salarioLiquido = salarioInssDeduzido - valorIr;
+
+return salarioLiquido;
+}
+
+console.log(calculoSalario(salario));
