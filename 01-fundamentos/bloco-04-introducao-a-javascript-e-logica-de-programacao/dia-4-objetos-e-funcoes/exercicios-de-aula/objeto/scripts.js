e264caf4fdb29365ceb5ae6fe14fc46fecae8847
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-25 16:38:29 -0300

    exercicio-4

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercicios-de-aula/objeto/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercicios-de-aula/objeto/scripts.js
@@ -1,11 +1,10 @@
 //Exercicio 1 e 2
-
 let player = {name: 'Marta', lastName: 'Silva', age: 34};
 
-console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' de idade.' );
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' de idade.');
 
 //Exercicio 3
-
 player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 
-console.log(player.bestInTheWorld);
+//Exercicio 4
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
