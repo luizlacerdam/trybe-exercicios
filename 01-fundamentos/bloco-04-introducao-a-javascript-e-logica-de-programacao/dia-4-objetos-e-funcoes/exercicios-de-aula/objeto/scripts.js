4d547783b13533b884840af6aabbd8d86d79a0e4
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-25 16:43:15 -0300

    exercicio-5

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercicios-de-aula/objeto/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercicios-de-aula/objeto/scripts.js
@@ -1,5 +1,5 @@
 //Exercicio 1 e 2
-let player = {name: 'Marta', lastName: 'Silva', age: 34};
+let player = {name: 'Marta', lastName: 'Silva', age: 34, medals: {golden: 2, silver: 3}};
 
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' de idade.');
 
@@ -8,3 +8,7 @@ player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 
 //Exercicio 4
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
+
+//Exercicio 5
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+
