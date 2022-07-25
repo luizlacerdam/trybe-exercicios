8b8aefe8f7fedb105ae481ca1231f4771e4113c0
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-25 18:01:08 -0300

    exercicio-2-e-3

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercicios-de-aula/functions/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercicios-de-aula/functions/scripts.js
@@ -2,29 +2,55 @@
 const a = 2;
 const b = 5;
 
-function add(num1, num2) {
-    return num1 + num2;
-}
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
 
-function sub(num1, num2) {
-    return num1 - num2;
-}
+// function sub(num1, num2) {
+//     return num1 - num2;
+// }
 
-function mult(num1, num2) {
-    return num1 * num2;
-}
+// function mult(num1, num2) {
+//     return num1 * num2;
+// }
 
-function div(num1, num2) {
-    return num1 / num2;
-}
+// function div(num1, num2) {
+//     return num1 / num2;
+// }
 
-function mod(num1, num2) {
-    return num1 % num2;
-}
-console.log(add(a,b));
-console.log(sub(a,b));
-console.log(mult(a,b));
-console.log(div(a,b));
-console.log(mod(a,b));
+// function mod(num1, num2) {
+//     return num1 % num2;
+// }
+// console.log(add(a,b));
+// console.log(sub(a,b));
+// console.log(mult(a,b));
+// console.log(div(a,b));
+// console.log(mod(a,b));
 
+//Exercicio 2
+// function maiorDeDois(num1, num2) {
+//     if (num1 > num2) {
+//         return num1;
+//     } else if (num2 > num1) {
+//         return num2;
+//     } else {
+//         return null;
+//     }
+// }
 
+// console.log(maiorDeDois(a,b));
+
+//Exercicio 3
+// function menorDeDois(num1, num2) {
+//     if (num1 < num2) {
+//         return num1;
+//     } else if (num2 < num1) {
+//         return num2;
+//     } else {
+//         return null;
+//     }
+// }
+
+// console.log(menorDeDois(a,b));
+
+//Exercicio 4
