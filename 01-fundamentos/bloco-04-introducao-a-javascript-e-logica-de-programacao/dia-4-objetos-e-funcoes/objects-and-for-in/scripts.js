2c7f02c9226968c226138c2a8bb344b3e409e8a6
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-25 20:42:17 -0300

    exercicios-pt1-1-a-5

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/objects-and-for-in/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/objects-and-for-in/scripts.js
@@ -5,19 +5,35 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-console.log('Bem Vinda, ' + info.personagem + '.');
+// console.log('Bem Vinda, ' + info.personagem + '.');
 
 //Exercicio 2
 info.recorrente = 'Sim';
 
-console.log(info.recorrente);
+// console.log(info.recorrente);
 
-//Exercicio 3
-for (let key in info) {
-    console.log(key);
-}
+// //Exercicio 3
+// for (let key in info) {
+//     console.log(key);
+// }
 
-//Exercicio 4
-for (let key in info) {
-    console.log(info[key]);
-}
+// //Exercicio 4
+// for (let key in info) {
+//     console.log(info[key]);
+// }
+
+//Exercicio 5
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
+
+console.log(info.personagem + ' e ' + info2.personagem);
+console.log(info.origem + ' e ' + info2.origem);
+console.log(info.nota + ' e ' + info2.nota);
+console.log('Ambos recorrentes');
+
+// Exercicio 6
