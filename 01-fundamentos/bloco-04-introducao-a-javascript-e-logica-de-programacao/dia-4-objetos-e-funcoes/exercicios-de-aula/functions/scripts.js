c006fbb31bc92468e872ab47049a88553f8b09bb
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-25 17:50:33 -0300

    exercicio-1

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercicios-de-aula/functions/scripts.js
@@ -0,0 +1,30 @@
+//Exercicio 1
+const a = 2;
+const b = 5;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+function mult(num1, num2) {
+    return num1 * num2;
+}
+
+function div(num1, num2) {
+    return num1 / num2;
+}
+
+function mod(num1, num2) {
+    return num1 % num2;
+}
+console.log(add(a,b));
+console.log(sub(a,b));
+console.log(mult(a,b));
+console.log(div(a,b));
+console.log(mod(a,b));
+
+
