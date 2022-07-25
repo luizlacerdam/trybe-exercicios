6ec700e99c6f019b5fe9076c8005f8807d42c2c5
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-25 20:55:50 -0300

    exercicios-pt1-1-a-8

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/objects-and-for-in/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/objects-and-for-in/scripts.js
@@ -37,3 +37,29 @@ console.log(info.nota + ' e ' + info2.nota);
 console.log('Ambos recorrentes');
 
 // Exercicio 6
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+//   console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+
+  //Exercicio 7
+  leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  });
+
+  console.log(leitor.livrosFavoritos[1]);
+
+  //Exercicio 8
+  console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
