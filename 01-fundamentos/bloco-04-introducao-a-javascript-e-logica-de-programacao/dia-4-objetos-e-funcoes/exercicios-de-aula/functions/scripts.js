e951875b688406a1673217932a2ac0b9f2c55cbe
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-25 18:42:33 -0300

    exercicio-9

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercicios-de-aula/functions/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercicios-de-aula/functions/scripts.js
@@ -137,3 +137,14 @@ const c = 300;
 //     }
 // }
 // console.log(umPar(a,b,c));
+
+//Exercicio 9
+function umImpar(num1,num2,num3) {
+    if (num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(umImpar(a,b,c));
