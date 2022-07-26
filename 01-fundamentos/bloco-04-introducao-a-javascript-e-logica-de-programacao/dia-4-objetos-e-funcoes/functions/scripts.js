0ec36344009811de6ab326988ed32a574ce4bac0
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-26 16:27:01 -0300

    execicio-3

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/functions/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/functions/scripts.js
@@ -1,36 +1,57 @@
 //Exercicio 1
-function isPalindromo (string) {
-    let palindromo = false;
+// function isPalindromo (string) {
+//     let palindromo = false;
 
-    for (let i = 0; i < (string.length - 1) / 2; i += 1) {
-        if (string[i] == string[string.length - i - 1]) {
-            palindromo = true;
-        } else {
-            palindromo = false;
-        }
-    }
-    return palindromo;
-}
+//     for (let i = 0; i < (string.length - 1) / 2; i += 1) {
+//         if (string[i] == string[string.length - i - 1]) {
+//             palindromo = true;
+//         } else {
+//             palindromo = false;
+//         }
+//     }
+//     return palindromo;
+// }
 
-console.log(isPalindromo('reger'));
+// console.log(isPalindromo('reger'));
 
-//Exercicio 2
-let arrayTest = [2, 3, 6, 7, 10, 1];
+// //Exercicio 2
+// let arrayTest = [2, 3, 6, 7, 10, 1];
 
-function maiorValor(array) {
-    let maior;
+// function maiorValor(array) {
+//     let maior;
+
+//     for (let i = 0; i < array.length; i += 1) {
+//         if (i == 0) {
+//             maior = array[i];
+//         } else {
+//             if (array[i] > maior) {
+//                 maior = array[i];
+//             }
+//         }
+        
+//     }
+//     return maior;    
+// }
+
+// console.log(maiorValor(arrayTest));
+
+//Exercicio 3
+let arrayTest = [2, 4, 6, 7, 10, 0, -3];
+
+function menorValor(array) {
+    let menor;
 
     for (let i = 0; i < array.length; i += 1) {
         if (i == 0) {
-            maior = array[i];
+            menor = array[i];
         } else {
-            if (array[i] > maior) {
-                maior = array[i];
+            if (array[i] < menor) {
+                menor = array[i];
             }
         }
         
     }
-    return maior;    
+    return menor;    
 }
 
-console.log(maiorValor(arrayTest));
+console.log(menorValor(arrayTest));
