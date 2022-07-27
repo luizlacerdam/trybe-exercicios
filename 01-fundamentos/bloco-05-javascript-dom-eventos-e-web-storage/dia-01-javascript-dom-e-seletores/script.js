59b431983b9be7b16bc8ce7f550803fec11b18f6
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-27 19:19:21 -0300

    emergency-tasks

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -1,1 +1,5 @@
 document.getElementById('header-container').style.backgroundColor = 'green';
+let emergencyTasks = document.getElementsByClassName('emergency-tasks');
+for (let i = 0; i < emergencyTasks.length; i += 1) {
+    emergencyTasks[i].style.backgroundColor = 'orange';
+}
