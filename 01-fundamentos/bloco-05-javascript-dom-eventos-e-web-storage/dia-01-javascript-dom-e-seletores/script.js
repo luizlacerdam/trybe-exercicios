7fbaf3bd42f665c401018fb91000f9d5da04f5f6
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-27 19:28:11 -0300

    emergency-tasks-h3

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -1,10 +1,18 @@
+//Mudando cor do header
 document.getElementById('header-container').style.backgroundColor = 'green';
+//Mudando cor das sections
 let emergencyTasks = document.getElementsByClassName('emergency-tasks');
 for (let i = 0; i < emergencyTasks.length; i += 1) {
     emergencyTasks[i].style.backgroundColor = 'orange';
 }
-
+//Mudando cor das sections
 let noEmergencyTasks = document.getElementsByClassName('no-emergency-tasks');
 for (let i = 0; i < noEmergencyTasks.length; i += 1) {
     noEmergencyTasks[i].style.backgroundColor = 'yellow';
+}
+//Mudando a cor dos h3
+let h3 = document.getElementsByTagName('h3');
+//Mudando a cor dos primeiros dois h3
+for (let i = 0; i < 2; i += 1) {
+   h3[i].style.backgroundColor = 'purple'; 
 }
