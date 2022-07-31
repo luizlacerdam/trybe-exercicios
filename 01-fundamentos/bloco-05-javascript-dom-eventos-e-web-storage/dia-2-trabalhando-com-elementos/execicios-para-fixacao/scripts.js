72921baba599ec2723bf41029386955529b4c151
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-31 09:40:24 -0300

    exercicio-2

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-trabalhando-com-elementos/execicios-para-fixacao/scripts.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-trabalhando-com-elementos/execicios-para-fixacao/scripts.js
@@ -3,3 +3,7 @@ let h1 = document.createElement('h1');
 //Exercicio 1
 h1.innerText = 'Exercício 5.2 - JavaScript DOM';
 body.appendChild(h1)
+//Exercicio 2
+let tag = document.createElement('main');
+tag.classList.add('main-content');
+body.appendChild(tag);
