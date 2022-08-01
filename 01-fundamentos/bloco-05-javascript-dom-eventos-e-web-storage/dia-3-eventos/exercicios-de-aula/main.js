8e324303ce7530b4cb4eba8dc8de648310591ac7
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-31 21:34:05 -0300

    exercicio-5

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-eventos/exercicios-de-aula/main.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-eventos/exercicios-de-aula/main.js
@@ -35,7 +35,9 @@ myWebpage.addEventListener('dblclick', function () {
 })
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
-
+myWebpage.addEventListener('mouseover', function () {
+  myWebpage.style.color = 'red'
+})
 // Segue abaixo um exemplo do uso de event.target:
 
 
