a1981c390f19dfae0c04721e1a1007d57c2c434e
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-16 19:30:57 -0300

    req-1-completo

--- 01-fundamentos/bloco-7-Introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/scripts.test.js
+++ 01-fundamentos/bloco-7-Introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/scripts.test.js
@@ -1,7 +1,20 @@
 const sum = require('./scripts');
 
 describe ('Testa valores da função sum:', () => {
-  test('dois mais dois é quatro', () => {
-    expect(2 + 2).toBe(4);
+  test('quatro mais cinco é nove', () => {
+    expect(sum(4,5)).toBe(9);
   });
+
+  test('zero mais zero é zero', () => {
+    expect(sum(0,0)).toBe(0);
+  });
+
+  test('verifica se há error quando há string', () => {
+    expect(() => sum(4,'5')).toThrow(Error);
+  });
+
+  test('verifica se há error quando há string', () => {
+    expect(() => sum(4,'5')).toThrow('parameters must be numbers');
+  });
+
 });
