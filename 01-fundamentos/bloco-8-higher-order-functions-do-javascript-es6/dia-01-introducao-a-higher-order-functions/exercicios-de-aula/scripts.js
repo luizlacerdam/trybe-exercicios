67a5666e1687df5c029a15fd1cd36076ee82ff6a
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-21 20:27:27 -0300

    exercicio-de-aula

---
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicios-de-aula/scripts.js
@@ -0,0 +1,13 @@
+// requisito 1
+const wakeUp = () => 'Acordando!!';
+
+// requisito 2
+const breakfast = () => 'Bora tomar café!!';
+
+// requisito 3
+const goSleep = () => 'Partiu dormir!!';
+
+// requisito 4
+const doingThings = (param) => console.log(param);
+
+doingThings(goSleep());
