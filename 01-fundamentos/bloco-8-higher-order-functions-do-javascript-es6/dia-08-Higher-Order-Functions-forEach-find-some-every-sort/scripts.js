717baf8495e0ff1c7f6e6e1b5d2af9acdaab72ef
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-22 18:25:08 -0300

    req-5

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-08-Higher-Order-Functions-forEach-find-some-every-sort/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-08-Higher-Order-Functions-forEach-find-some-every-sort/scripts.js
@@ -80,19 +80,27 @@ function smallerName(array) {
   // Variável nameBook que receberá o valor do menor nome;
   return nameBook;
 }
-console.log(smallerName(books));
+//console.log(smallerName(books));
 
 // requisito 3
 function getNamedBook(array) {
   // escreva seu código aqui
   return array.find((element) => element.name.length === 26);
 }
-console.log(getNamedBook(books));
+//console.log(getNamedBook(books));
 
 // requisito 4
 function booksOrderedByReleaseYearDesc(arr) {
   // escreva aqui seu código
   arr.sort((a, b) => a.releaseYear - b.releaseYear);
 }
-booksOrderedByReleaseYearDesc(books);
-console.log(books);
+// booksOrderedByReleaseYearDesc(books);
+// console.log(books);
+
+// requisito 5
+function everyoneWasBornOnSecXX(arr) {
+  // escreva seu código aqui
+  return arr.every((element) => element.author.birthYear >= 1900 && element.author.birthYear < 2000);
+
+}
+console.log(everyoneWasBornOnSecXX(books));
