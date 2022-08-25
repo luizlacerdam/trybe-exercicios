6ac9b65f64692e5565f31e0cce203803a2298a9e
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-25 18:55:50 -0300

    req-6

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-04-Higher-Order-Functions-reduce/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-04-Higher-Order-Functions-reduce/scripts.js
@@ -131,4 +131,22 @@ function containsA(arr) {
   }, 0), 0);
 }
 
-console.log(containsA(names));
+//console.log(containsA(names));
+// requisito 6
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage(estudantes, notas) {
+  // escreva seu código aqui
+  return estudantes.map((estudante, index) => ({
+    name: estudante, 
+    average: notas[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length,
+  }
+  ));
+}
+console.log(studentAverage(students, grades));
+// const expected = [
+//   { name: 'Pedro Henrique', average: 7.8 },
+//   { name: 'Miguel', average: 9.2 },
+//   { name: 'Maria Clara', average: 8.8 },
+// ];
