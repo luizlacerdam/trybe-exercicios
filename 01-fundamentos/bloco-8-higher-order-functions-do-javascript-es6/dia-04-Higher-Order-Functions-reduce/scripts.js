1f27ae15a699091a658cfb1f4b4de56a83ed910c
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-25 12:50:28 -0300

    req-2

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-04-Higher-Order-Functions-reduce/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-04-Higher-Order-Functions-reduce/scripts.js
@@ -1,11 +1,87 @@
-// requisito 1
-const arrays = [
-  ['1', '2', '3'],
-  [true],
-  [4, 5, 6],
+// // requisito 1
+// const arrays = [
+//   ['1', '2', '3'],
+//   [true],
+//   [4, 5, 6],
+// ];
+
+// function flatten(arr) { 
+//   return arr.reduce((acc, curr) => acc.concat(curr));
+// }
+//console.log(flatten(arrays));
+
+// requisito 2 dúvida no formato do ponto final e itens separados por vírgula 
+
+// function reduceNames() {
+//   return books.reduce((acc, book) =>  `${acc} ${book.author.name}.`, '')
+// }
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
 ];
 
-function flatten(arr) { 
-  return arr.reduce((acc, curr) => acc.concat(curr));
+//const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+
+function reduceNames(arr) {
+  return arr.reduce(((acc,curr) => `${acc} ${curr.name},`), '');
 }
-console.log(flatten(arrays));
+console.log(reduceNames(books));
+
