304c6cbf71e111895285057e5db856c1f0c0106c
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-26 20:38:14 -0300

    req-5

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/exercicios-do-dia/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/exercicios-do-dia/scripts.js
@@ -46,4 +46,15 @@ const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
 
 // Produza o mesmo resultado acima, porém utilizando array destructuring
 const [ola, saudacao] = saudacoes;
-saudacao(ola);
+//saudacao(ola);
+
+// requisito 2
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+//console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+[comida, animal, bebida] = [bebida, comida, animal];
+// console.log(comida, animal, bebida);
