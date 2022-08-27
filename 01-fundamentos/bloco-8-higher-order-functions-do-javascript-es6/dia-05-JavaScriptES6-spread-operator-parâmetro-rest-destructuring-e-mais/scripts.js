55697fc7b5767ae6623f69b7f2dc769ef612f954
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-27 16:50:22 -0300

    req-4

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/scripts.js
@@ -37,5 +37,41 @@ const personLikes = ({name, age, likes}) => `${name} is ${age} years old and lik
 // <nome> tem <anos de idade> e gosta de <gostos da pessoa> -->
 
 // Retornos esperados: -->
-console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
-console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+// console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+// console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+// requsito 4
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+// bornIn: nascido em
+
+// escreva filterPeople abaixo
+
+const filterPeople = (arr) => arr.filter((element) => element.bornIn < 2000 && element.nationality === 'Australian');
+console.log(filterPeople(people));
