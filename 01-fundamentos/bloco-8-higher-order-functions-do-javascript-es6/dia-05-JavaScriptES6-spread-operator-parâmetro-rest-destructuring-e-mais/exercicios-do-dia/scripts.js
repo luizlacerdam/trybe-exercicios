e8651735499133f06aead35cf1e84b5f9fbf823d
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-26 21:15:24 -0300

    req-8

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/exercicios-do-dia/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/exercicios-do-dia/scripts.js
@@ -75,11 +75,19 @@ let bebida = 'arroz';
 // };
 
 // console.log(getNationality(otherPerson)); // Ivan is Russian
-// console.log(getNationality(person));
+// // console.log(getNationality(person));
 
-const getPosition = (latitude, longitude) => ({
-  latitude,
-  longitude,
-});
+// const getPosition = (latitude, longitude) => ({
+//   latitude,
+//   longitude,
+// });
 
-console.log(getPosition(-19.8157, -43.9542));
+// console.log(getPosition(-19.8157, -43.9542));
+
+
+const multiply = (number, value = 1) => {
+  // Escreva aqui a sua função
+  return number * value;
+};
+
+console.log(multiply(8,2));
