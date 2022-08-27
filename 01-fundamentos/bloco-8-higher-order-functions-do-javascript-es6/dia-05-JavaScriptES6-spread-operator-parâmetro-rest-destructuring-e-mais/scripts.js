41de76d88845d7d058b7184064b8a9cd3d6e9b3c
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-27 17:20:54 -0300

    req-8

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/scripts.js
@@ -117,6 +117,13 @@ const ships = [
 const shipLength = (arr) => `${arr.name} is ${arr.length} ${arr.measurementUnit} long` 
 // escreva shipLength abaixo
 // retorno esperado
-console.log(shipLength(ships[0])) // 'Titanic is 269.1 meters long'
-console.log(shipLength(ships[1])) // 'Queen Mary 2 is 1132 feet long'
-console.log(shipLength(ships[2])) // 'Yamato is 256 meters long'
+// console.log(shipLength(ships[0])) // 'Titanic is 269.1 meters long'
+// console.log(shipLength(ships[1])) // 'Queen Mary 2 is 1132 feet long'
+// console.log(shipLength(ships[2])) // 'Yamato is 256 meters long'
+
+// requisito 8
+// escreva greet abaixo
+const greet = (nome, outros = 'Hi') => `${outros} ${nome}`
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
