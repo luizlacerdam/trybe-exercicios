5a48cbf01cda144a071773d5258ff3f66d77b9fe
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-27 16:14:10 -0300

    req-2

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/scripts.js
@@ -1,11 +1,15 @@
 // requisito 1
-const rectangleArea = (width, height) => width * height;
+// const rectangleArea = (width, height) => width * height;
 
-const rectangle1 = [1, 2];
-const rectangle2 = [3, 5];
-const rectangle3 = [6, 9];
-const rectangles = [rectangle1, rectangle2, rectangle3];
+// const rectangle1 = [1, 2];
+// const rectangle2 = [3, 5];
+// const rectangle3 = [6, 9];
+// const rectangles = [rectangle1, rectangle2, rectangle3];
 
-rectangles.forEach((rectangle) => {
-  console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
-});
+// rectangles.forEach((rectangle) => {
+//   console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
+// });
+
+// requisito 2
+const sum = ((...numeros) => numeros.reduce((acc, curr) => acc + curr,0));
+console.log(sum(4, 5, 6));
