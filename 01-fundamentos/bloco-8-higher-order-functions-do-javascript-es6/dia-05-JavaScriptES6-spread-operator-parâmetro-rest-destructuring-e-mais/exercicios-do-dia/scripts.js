fca028f7c5f51c36f796d4ae4d5db65393afdfae
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-26 21:10:28 -0300

    req-7

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/exercicios-do-dia/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/exercicios-do-dia/scripts.js
@@ -61,18 +61,25 @@ let bebida = 'arroz';
 
 // Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
 
-const getNationality = ({ firstName, nationality = 'Brazillian' }) => `${firstName} is ${nationality}`;
+// const getNationality = ({ firstName, nationality = 'Brazillian' }) => `${firstName} is ${nationality}`;
 
-const person = {
-  firstName: 'João',
-  lastName: 'Jr II',
-};
+// const person = {
+//   firstName: 'João',
+//   lastName: 'Jr II',
+// };
 
-const otherPerson = {
-  firstName: 'Ivan',
-  lastName: 'Ivanovich',
-  nationality: 'Russian',
-};
+// const otherPerson = {
+//   firstName: 'Ivan',
+//   lastName: 'Ivanovich',
+//   nationality: 'Russian',
+// };
 
-console.log(getNationality(otherPerson)); // Ivan is Russian
-console.log(getNationality(person));
+// console.log(getNationality(otherPerson)); // Ivan is Russian
+// console.log(getNationality(person));
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+console.log(getPosition(-19.8157, -43.9542));
