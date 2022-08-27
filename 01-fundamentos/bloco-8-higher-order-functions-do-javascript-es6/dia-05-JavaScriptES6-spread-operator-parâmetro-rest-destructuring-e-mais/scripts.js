1ca9564322540483132a5624be3a1b1302f43b28
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-27 16:46:38 -0300

    req-3

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/scripts.js
@@ -12,4 +12,30 @@
 
 // requisito 2
 const sum = ((...numeros) => numeros.reduce((acc, curr) => acc + curr,0));
-console.log(sum(4, 5, 6));
+//console.log(sum(4, 5, 6));
+
+// requsito 3
+// name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade -->
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+
+const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+// <nome> tem <anos de idade> e gosta de <gostos da pessoa> -->
+
+// Retornos esperados: -->
+console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
