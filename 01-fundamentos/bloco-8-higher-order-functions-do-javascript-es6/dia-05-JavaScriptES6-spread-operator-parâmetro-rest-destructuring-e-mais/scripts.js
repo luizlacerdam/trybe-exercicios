974e935096722eec394fa7858a7bf0a3fb5e6b85
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-27 17:17:04 -0300

    req-7

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-05-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/scripts.js
@@ -92,4 +92,31 @@ const toObject = ([marca, modelo, ano]) => ({
   modelo,
   ano,
 });
-console.log(toObject(palio));
+//console.log(toObject(palio));
+
+// requsito 7
+const ships = [
+  {
+    name: 'Titanic',
+    length: 269.1,
+    measurementUnit: 'meters',
+  },
+  {
+    name: 'Queen Mary 2',
+    length: 1132,
+    measurementUnit: 'feet',
+  },
+  {
+    name: 'Yamato',
+    length: 256,
+    measurementUnit: 'meters',
+  },
+];
+// measurementUnit: unidade de medida
+
+const shipLength = (arr) => `${arr.name} is ${arr.length} ${arr.measurementUnit} long` 
+// escreva shipLength abaixo
+// retorno esperado
+console.log(shipLength(ships[0])) // 'Titanic is 269.1 meters long'
+console.log(shipLength(ships[1])) // 'Queen Mary 2 is 1132 feet long'
+console.log(shipLength(ships[2])) // 'Yamato is 256 meters long'
