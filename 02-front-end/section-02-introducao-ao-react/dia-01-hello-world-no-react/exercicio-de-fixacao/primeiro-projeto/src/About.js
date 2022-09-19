5e94254e71747779989fb0fd8f2d3fa823a1ce45
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-09-19 19:04:07 -0300

    finalizando-exercicio-inicial

---
+++ 02-front-end/section-02-introducao-ao-react/dia-01-hello-world-no-react/exercicio-de-fixacao/primeiro-projeto/src/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+class aboutme extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Luiz Lacerda</h1>
+        <p>Sobre mim</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>HTML</li>
+          <li>JavaScript</li>
+          <li>CSS</li>
+          <li>Python</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default aboutme;
