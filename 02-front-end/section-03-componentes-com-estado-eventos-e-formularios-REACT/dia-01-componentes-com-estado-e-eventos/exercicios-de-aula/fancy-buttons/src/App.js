35913710e62b999a4b2a90d3ed24f2504957e89e
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-09-22 16:16:35 -0300

    loging-com-funcao-fora

--- 02-front-end/section-03-componentes-com-estado-eventos-e-formularios-REACT/dia-01-componentes-com-estado-e-eventos/exercicios-de-aula/fancy-buttons/src/App.js
+++ 02-front-end/section-03-componentes-com-estado-eventos-e-formularios-REACT/dia-01-componentes-com-estado-e-eventos/exercicios-de-aula/fancy-buttons/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 
+function greeting(event) {
+  console.log(event.target.innerText)
+}
+
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-     <button>Hi</button>
+
+    <div className="App">
+      <p><button onClick={greeting}>Hi!</button></p>
+      <p><button onClick={greeting}>Hello!</button></p>
+      <p><button onClick={greeting}>Howdy!</button></p>
     </div>
     );
   }
