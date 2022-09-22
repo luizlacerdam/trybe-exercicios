829d79a30863af6d3f117d3a70b49f08b5ae98e6
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-09-22 18:36:29 -0300

    exercicio-3

--- 02-front-end/section-03-componentes-com-estado-eventos-e-formularios-REACT/dia-01-componentes-com-estado-e-eventos/exercicios-de-aula/fancy-buttons/src/App.js
+++ 02-front-end/section-03-componentes-com-estado-eventos-e-formularios-REACT/dia-01-componentes-com-estado-e-eventos/exercicios-de-aula/fancy-buttons/src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import './App.css';
 
-function greeting(event) {
-  console.log(event.target.innerText)
-}
-
 class App extends React.Component {
+  constructor() {
+    super();
+    this.greeting = this.greeting.bind(this);
+    this.state = {
+      Hi: 0,
+      Hello: 0,
+      Howdy: 0,
+    };
+  }
+  
+  greeting(event) {
+    const nomeBotao = event.target.innerText;
+    this.setState((estadoAnterior) => ({
+      [nomeBotao]: estadoAnterior[nomeBotao] + 1,
+    }));
+    console.log(event.target.innerText)
+  }
   render() {
     return (
-
     <div className="App">
-      <p><button onClick={greeting}>Hi!</button></p>
-      <p><button onClick={greeting}>Hello!</button></p>
-      <p><button onClick={greeting}>Howdy!</button></p>
+      <p>
+        <button onClick={ this.greeting }>Hi</button>
+        <span> { this.state.Hi }</span>
+      </p>
+      <p>
+        <button onClick={ this.greeting }>Hello</button>
+        <span> { this.state.Hello }</span>
+      </p>
+      <p>
+        <button onClick={ this.greeting }>Howdy</button>
+        <span> { this.state.Howdy }</span>
+      </p>
     </div>
     );
   }
