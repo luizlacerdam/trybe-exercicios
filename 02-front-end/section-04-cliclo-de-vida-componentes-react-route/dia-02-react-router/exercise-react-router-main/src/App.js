87635380c17142a53ad36c09d419ca70e47de3e0
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-10-02 10:55:16 -0300

    requisito-6

--- 02-front-end/section-04-cliclo-de-vida-componentes-react-route/dia-02-react-router/exercise-react-router-main/src/App.js
+++ 02-front-end/section-04-cliclo-de-vida-componentes-react-route/dia-02-react-router/exercise-react-router-main/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
 import Users from './components/Users';
@@ -11,6 +11,9 @@ class App extends Component {
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/users" component={Users} />
+      <Link to="/">Home</ Link>
+      <Link to="/about">About</ Link>
+      <Link to="/users">Users</ Link>
       </BrowserRouter>
     );
   }
