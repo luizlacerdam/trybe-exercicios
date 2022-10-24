494bc16caa1ae6f90162e4604b51efe2ea299e85
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-10-24 18:18:06 -0300

    req-2

--- 02-front-end/section-07-redux/dia-01-redux-intro-com-javascript/exercicios-do-dia/index.js
+++ 02-front-end/section-07-redux/dia-01-redux-intro-com-javascript/exercicios-do-dia/index.js
@@ -2,4 +2,13 @@ import { legacy_createStore as createStore } from "redux";
 
 
 
+const reducer = (state, action) => {
+  switch (action) {
+  
+    default:
+      return state;
+  }
+  
+}
+
 const store = createStore(reducer);
