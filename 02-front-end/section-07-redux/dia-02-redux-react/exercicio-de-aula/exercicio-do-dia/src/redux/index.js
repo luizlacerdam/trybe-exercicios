0ea6d99637cfa611218031c0326ebd0f66e332e2
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-10-25 15:09:18 -0300

    compose-with-devtools

--- 02-front-end/section-07-redux/dia-02-redux-react/exercicio-de-aula/exercicio-do-dia/src/redux/index.js
+++ 02-front-end/section-07-redux/dia-02-redux-react/exercicio-de-aula/exercicio-do-dia/src/redux/index.js
@@ -1,7 +1,8 @@
+import { composeWithDevTools } from "@redux-devtools/extension";
 import { legacy_createStore as createStore } from "redux";
 const INIT_STATE = { count: 0 };
 
 const reducer = (state = INIT_STATE, action) => state;
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 export default store;
