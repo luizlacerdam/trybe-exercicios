2e921a43bc3f31a50cde88bc2976ae92b22f3877
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-10-28 15:41:26 -0300

    main-index

--- 02-front-end/section-07-redux/dia-05-test-react-redux/exercicios-de-aula/contador-de-cliques/src/App.test.js
+++ 02-front-end/section-07-redux/dia-05-test-react-redux/exercicios-de-aula/contador-de-cliques/src/App.test.js
@@ -1,5 +1,22 @@
-import { render, screen } from '@testing-library/react';
+// src/App.test.js
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, cleanup, screen } from '@testing-library/react';
 import App from './App';
+// src/App.test.js
+import {  combineReducers, legacy_createStore } from 'redux';
+import clickReducer from './reducers';
+
+const renderWithRedux = (
+  component,
+  {
+    initialState,
+    store = legacy_createStore(combineReducers({ clickReducer }), initialState),
+  } = {}
+) => ({
+  ...render(<Provider store={store}>{component}</Provider>),
+  store,
+});
 
 test('renders learn react link', () => {
   render(<App />);
