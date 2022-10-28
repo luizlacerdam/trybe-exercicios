fcb301bb22f3a388dd8fd39c0cc052e38cb9a2f0
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-10-28 13:47:02 -0300

    store

--- 02-front-end/section-07-redux/dia-04-actions-assicronas/game-of-trunks/src/redux/index.js
+++ 02-front-end/section-07-redux/dia-04-actions-assicronas/game-of-trunks/src/redux/index.js
@@ -1,7 +1,7 @@
 import { composeWithDevTools } from "@redux-devtools/extension";
 import { legacy_createStore } from "redux";
-import reducer from "./reducers";
+import gotReducer from "./reducers";
 
-const store = legacy_createStore(reducer, composeWithDevTools())
+const store = legacy_createStore(gotReducer, composeWithDevTools())
 
 export default store;
