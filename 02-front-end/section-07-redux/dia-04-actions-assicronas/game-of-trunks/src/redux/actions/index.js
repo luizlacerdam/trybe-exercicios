0e05d06eeb829bf94a710278739252a6ffffcce6
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-10-28 14:33:27 -0300

    actions-fetchGot

---
+++ 02-front-end/section-07-redux/dia-04-actions-assicronas/game-of-trunks/src/redux/actions/index.js
@@ -0,0 +1,31 @@
+const GET_DATA = 'GET_DATA';
+const REQUEST_DATA = 'REQUEST_DATA';
+const FAILED_REQUEST = 'FAILED_REQUEST';
+
+function requestData() {
+    return {type: REQUEST_DATA}
+}
+
+function getData(json) {
+    return {
+        type: GET_DATA, 
+        payload: json.message,
+    }
+}
+
+function failedRequest(error) {
+    return { 
+      type: FAILED_REQUEST, 
+      payload: error,
+    };
+}
+
+export function fetchGot(name) {
+    return (dispatch) => {
+        dispatch(requestData());
+        return fetch(`https://anapioficeandfire.com/api/characters?name=${name}`)
+        .then((response) => response.json())
+        .then((json) => dispatch(getData(json)))
+        .catch((error) => dispatch(failedRequest(error)))
+    }
+}
