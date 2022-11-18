c4763e980897a27ce5636539e25cd5483d5a0bd6
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-11-18 13:42:11 -0300

    HTML-done

--- 02-front-end/section-09-context-api-react-hooks/day-01-react-hooks-useState-useEffect/exercise-1/form-with-hook/src/components/Form.js
+++ 02-front-end/section-09-context-api-react-hooks/day-01-react-hooks-useState-useEffect/exercise-1/form-with-hook/src/components/Form.js
@@ -2,7 +2,39 @@ import React from "react";
 
 function Form() {
     return(
-        <div>Hello</div>
+        <form>
+            <label htmlFor="name">
+                Full Name
+                <input id="name" type="text" />
+            </label>
+            <br />
+            <label htmlFor="age">
+                Age
+                <input id="age" type="number" />
+            </label>
+            <br />
+            <label htmlFor="city">
+                City
+                <input id="city" type="text" />
+            </label>
+            <label htmlFor="fund">
+                Fund
+                <input type="radio" name="" id="fund" />
+            </label>
+            <label htmlFor="FrontEnd">
+                FrontEnd
+                <input type="radio" name="" id="FrontEnd" />
+            </label>
+            <label htmlFor="BackEnd">
+                BackEnd
+                <input type="radio" name="" id="BackEnd" />
+            </label>
+            <label htmlFor="SC">
+                SC
+                <input type="radio" name="" id="SC" />
+            </label>
+            <button type="button">Send</button>
+        </form>
     )
 }
 
