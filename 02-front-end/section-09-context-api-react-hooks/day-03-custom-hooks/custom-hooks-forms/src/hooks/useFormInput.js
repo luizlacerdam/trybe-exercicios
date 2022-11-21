89ca1f82e8c818ca9a4aecdf51a41e5f0b9ab993
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-11-21 18:50:55 -0300

    exercise

---
+++ 02-front-end/section-09-context-api-react-hooks/day-03-custom-hooks/custom-hooks-forms/src/hooks/useFormInput.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  function clear() {
+    setValue('')
+  }
+
+  return {
+    value: value,
+    onChange: handleChange,
+    clear,
+  };
+
+}
+
+export default useFormInput;
