b031a3553efc6deaa08e22cf0da0c6b2884b5782
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-02 15:47:49 -0300

    ex-04

--- 03-back-end/section-04-node.js/day-01-async-runtime/exercicios/bmi.js
+++ 03-back-end/section-04-node.js/day-01-async-runtime/exercicios/bmi.js
@@ -2,7 +2,7 @@ const readline = require('readline-sync');
 
 const bmi = () => {
 
-    const weight = readline.questionInt('What is your body weight? (kg) ');
+    const weight = readline.questionFloat('What is your body weight? (kg) ');
     const height = readline.questionInt('What is your height? (cm) ');
 
     const result = weight / (height ^ 2);
