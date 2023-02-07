18179c91857ff2cb6e9997e72751c879340990b6
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-06 23:57:10 -0300

    exercicio-api-completo

--- 03-back-end/section-04-node.js/day-03-tests-mocha-chai-sinon/exercicios-de-aula/src/app.js
+++ 03-back-end/section-04-node.js/day-03-tests-mocha-chai-sinon/exercicios-de-aula/src/app.js
@@ -24,5 +24,33 @@ const writeFile = async () => {
     }
 };
 
-readFile().then((data) => console.log(data))
+app.get('/chocolates', async (req, res) => {
+    try {
+        const chocolates = await readFile();
+        res.status(200).json(chocolates.chocolates);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+app.get('/chocolates/:id', async (req, res) => {
+    try {
+        const chocolates = await readFile();
+        const chocolate = chocolates.chocolates.find(({ id }) => id === Number(req.params.id));
+        res.status(200).json(chocolate);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+app.get('/chocolates/brand/:brandId', async (req, res) => {
+    try {
+        const chocolates = await readFile();
+        const chocolate = chocolates.brands.find(({ id }) => id === Number(req.params.brandId));
+        res.status(200).json(chocolate);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
 module.exports = app;
