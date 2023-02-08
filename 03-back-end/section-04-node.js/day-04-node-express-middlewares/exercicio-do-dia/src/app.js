4b8de22d76a6ce497317fb657315ba507ba9da09
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-08 14:51:06 -0300

    middleswares

--- 03-back-end/section-04-node.js/day-04-node-express-middlewares/exercicio-do-dia/src/app.js
+++ 03-back-end/section-04-node.js/day-04-node-express-middlewares/exercicio-do-dia/src/app.js
@@ -1,10 +1,12 @@
 // src/app.js
 const express = require('express');
-const validateActivities = require('./middlewares/validateActivities')
+const validateName = require('./middlewares/validateName');
+const validatePrice = require('./middlewares/validatePrice');
+const validateDesc = require('./middlewares/validateDesc');
 const app = express();
 app.use(express.json())
 
-app.post('/activities', validateActivities, (req, res) => {
+app.post('/activities', validateName, validatePrice, validateDesc, (req, res) => {
   res.status(201).json({ "message": "Atividade cadastrada com sucesso!" });
 });
 
