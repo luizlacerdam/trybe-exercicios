1e810b8de68da3a2c061ed72bc83195b06ae14c6
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-28 10:44:19 -0300

    create-book

--- 03-back-end/section-06-ORM-Auth/day-01-ORM-db/exercicios-do-dia/services/book.service.js
+++ 03-back-end/section-06-ORM-Auth/day-01-ORM-db/exercicios-do-dia/services/book.service.js
@@ -3,14 +3,20 @@ const { Book } = require('../models/');
 const getAll = async () => {
     const books = await Book.findAll();
     return books;
-}
+};
 
 const getById = async (id) => {
     const book = await Book.findByPk(id);
     return book;
-}
+};
+
+const create = async (title, author, pageQuantity) => {
+    const book = await Book.create({ title, author, pageQuantity });
+    return book;
+};
 
 module.exports = {
     getAll,
     getById,
+    create,
 };
