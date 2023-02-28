d3980cdea21c218faf4201f02a37b1f4ad39ea6d
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-28 10:34:01 -0300

    getAll

--- 03-back-end/section-06-ORM-Auth/day-01-ORM-db/exercicios-do-dia/index.js
+++ 03-back-end/section-06-ORM-Auth/day-01-ORM-db/exercicios-do-dia/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const BooksController = require('./controllers/book.controller'); // importamos o controller aqui
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.get('/books', BooksController.getAll);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
