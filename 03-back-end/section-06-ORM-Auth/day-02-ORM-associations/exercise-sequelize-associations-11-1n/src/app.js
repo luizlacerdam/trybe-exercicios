ffdd04bc46f6ef9be6d301e5c036af0f375dffa2
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-02 20:32:25 -0300

    reqs-08

--- 03-back-end/section-06-ORM-Auth/day-02-ORM-associations/exercise-sequelize-associations-11-1n/src/app.js
+++ 03-back-end/section-06-ORM-Auth/day-02-ORM-associations/exercise-sequelize-associations-11-1n/src/app.js
@@ -7,8 +7,10 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const { getAccountById, getAccountByIdLazy } = require('./controllers/account');
+const { getCommentsByAccountId } = require('./controllers/comment');
 
 app.get('/accounts/:id', getAccountById);
 app.get('/accounts-v2/:id', getAccountByIdLazy);
+app.get('/accounts/:id/comments', getCommentsByAccountId);
 
 module.exports = app;
