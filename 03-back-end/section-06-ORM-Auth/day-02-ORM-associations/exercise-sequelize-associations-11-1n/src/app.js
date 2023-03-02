171b879f111347f5a5f36b421451deb19d160a57
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-02 20:23:13 -0300

    reqs-07

--- 03-back-end/section-06-ORM-Auth/day-02-ORM-associations/exercise-sequelize-associations-11-1n/src/app.js
+++ 03-back-end/section-06-ORM-Auth/day-02-ORM-associations/exercise-sequelize-associations-11-1n/src/app.js
@@ -6,8 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const { getAccountById } = require('./controllers/account');
+const { getAccountById, getAccountByIdLazy } = require('./controllers/account');
 
 app.get('/accounts/:id', getAccountById);
+app.get('/accounts-v2/:id', getAccountByIdLazy);
 
 module.exports = app;
--- 03-back-end/section-06-ORM-Auth/day-02-ORM-associations/exercise-sequelize-associations-11-1n/src/controllers/account.js
+++ 03-back-end/section-06-ORM-Auth/day-02-ORM-associations/exercise-sequelize-associations-11-1n/src/controllers/account.js
@@ -18,6 +18,24 @@ const getAccountById = async (req, res) => {
       }
 };
 
+const getAccountByIdLazy = async (req, res) => {
+    try {
+      const listOfAccounts = await Account.findAll({
+        where: { id: req.params.id },
+      });
+  
+      if (!listOfAccounts.length) {
+        return res.status(404).send({ message: 'Nenhuma conta cadastrada' });
+      }
+  
+      return res.status(200).json(listOfAccounts[0]);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: 'Algo deu errado!' });
+    }
+  };
+
 module.exports = {
     getAccountById,
+    getAccountByIdLazy,
   };
