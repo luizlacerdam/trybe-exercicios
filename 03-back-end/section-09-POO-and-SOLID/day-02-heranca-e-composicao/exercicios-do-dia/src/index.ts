6e77cd3aa75ed283efd0babec3307c4c9595b9b0
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-25 14:47:24 -0300

    Teacher-Subject-classes

--- 03-back-end/section-09-POO-and-SOLID/day-02-heranca-e-composicao/exercicios-do-dia/src/index.ts
+++ 03-back-end/section-09-POO-and-SOLID/day-02-heranca-e-composicao/exercicios-do-dia/src/index.ts
@@ -48,7 +48,9 @@ class Student extends Person {
 
     constructor(name: string, birthDate: Date) {
         super(name, birthDate)
-        this.enrollment = this.generateEnrollment();
+        this._enrollment = this.generateEnrollment();
+        this._examsGrades = [];
+        this._assignmentsGrades = [];
     }
 
     get enrollment(): string {
@@ -107,15 +109,102 @@ class Student extends Person {
 }
 
 interface Employee {
-    registration: number;
+    registration: string;
     salary: number;
     admissionDate: Date;
     generateRegistration(): string;
 }
 
+class Subject {
+    constructor(private _name: string) {
+        this.name = _name; // necessário para usar a validação
+    }
 
-const maria = new Person('Msdds', new Date('1980/01/25'));
-const luiza = new Person('Luiza Andrade', new Date('2005/10/02'));
+    get name() {
+        return this._name;
+    }
 
-console.log(maria);
-console.log(luiza);
+    set name(v: string) {
+        this.validationName(v)
+        this._name = v;
+    }
+
+    private validationName(v: string): void {
+        if (v.length < 3) throw new Error('O nome tem que possuir no mínimo 3 caracteres.');
+    }
+
+}
+
+const math = new Subject('Ma');
+const story = new Subject('História');
+const philosophy = new Subject('Filosofia');
+
+//console.log(math);
+console.log(story.name);
+console.log(philosophy);
+
+class Teacher extends Person implements Employee {
+    private _subject: Subject;
+    private _registration = String();
+    private _admissionDate: Date;
+
+    constructor(name: string, birthDate: Date, private _salary: number, subject: Subject) {
+        super(name, birthDate);
+
+        this._subject = subject;
+        this.salary = _salary;
+        this._admissionDate = new Date();
+        this.registration = this.generateRegistration();
+    }
+
+    get subject(): Subject {
+        return this._subject;
+    }
+
+    set subject(value: Subject) {
+        this._subject = value;
+    }
+
+    get registration(): string {
+        return this._registration;
+    }
+
+    set registration(value: string) {
+        if (value.length < 16) throw new Error('O registro deve possuir no mínimo 16 caracteres.');
+
+        this._registration = value;
+    }
+
+    get salary(): number {
+        return this._salary;
+    }
+
+    set salary(value: number) {
+        if (value < 0) throw new Error('O salário não pode ser negativo.');
+
+        this._salary = value;
+    }
+
+    get admissionDate(): Date {
+        return this._admissionDate;
+    }
+
+    set admissionDate(value: Date) {
+        if (value.getTime() > new Date().getTime()) throw new Error('A data de admissão não pode ser uma data no futuro.');
+
+        this._admissionDate = value;
+    }
+
+    generateRegistration(): string {
+        const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+
+        return `PRF${randomStr}`;
+    }
+}
+
+// const maria = new Person('Msdds', new Date('1980/01/25'));
+// const luiza = new Person('Luiza Andrade', new Date('2005/10/02'));
+
+// console.log(maria);
+// console.log(luiza);
+
