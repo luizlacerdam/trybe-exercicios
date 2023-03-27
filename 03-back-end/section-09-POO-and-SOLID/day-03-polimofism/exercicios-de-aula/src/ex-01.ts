c96837758aac03953dd7c1e8ee7b6ccaadb14c4e
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-27 09:16:27 -0300

    ex-01-polimofism

---
+++ 03-back-end/section-09-POO-and-SOLID/day-03-polimofism/exercicios-de-aula/src/ex-01.ts
@@ -0,0 +1,36 @@
+abstract class Character {
+    abstract talk(): void
+    abstract specialMove(): void
+}
+
+class MeleeCharacter extends Character {
+    constructor(private _n: string, private _sMoviment: string) {
+        super();
+    }
+    talk(): void {
+        console.log(`${this._n} is melee and is talking.`);
+    }
+    specialMove(): void {
+        console.log(`${this._n} used ${this._sMoviment}`);
+    }
+}
+
+class LongRangeCharacter extends Character {
+    constructor(private _n: string, private _sMoviment: string) {
+        super();
+    }
+    talk(): void {
+        console.log(`${this._n} is ranged and is talking.`);
+    }
+    specialMove(): void {
+        console.log(`${this._n} used ${this._sMoviment}`);
+    }
+}
+
+const yoshi = new MeleeCharacter('Yoshi', 'Super dragon');
+const samus = new LongRangeCharacter('Samus', 'Zero Laser');
+
+yoshi.talk();
+yoshi.specialMove();
+samus.talk();
+samus.specialMove();
