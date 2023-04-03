385e2434e655a7fba525a4de2b425609ba73ffac
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-03 16:02:53 -0300

    ocp-exercicio-de-aula

--- 03-back-end/section-09-POO-and-SOLID/day-04-SOLID/exercicios-de-aula/Open-Closed-principle/src/index.ts
+++ 03-back-end/section-09-POO-and-SOLID/day-04-SOLID/exercicios-de-aula/Open-Closed-principle/src/index.ts
@@ -2,18 +2,5 @@ export default function progressNotification(
   message: string,
   notificationType: string,
 ): void {
-  switch (notificationType) {
-    case 'Email':
-      console.log('Email: ', message);
-      break;
-    case 'Phone':
-      console.log('Phone: ', message);
-      break;
-    case 'Console':
-      console.log('Console: ', message);
-      break;
-    default:
-      console.log(message);
-      break;
-  }
+  console.log(`${notificationType}: ${message}`);
 }
