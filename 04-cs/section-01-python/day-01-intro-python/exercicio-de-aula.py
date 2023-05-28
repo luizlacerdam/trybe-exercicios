ef83cad1cc585794ae7f35b10427c678f1548ec8
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-28 20:04:57 -0300

    intro-python

---
+++ 04-cs/section-01-python/day-01-intro-python/exercicio-de-aula.py
@@ -0,0 +1,15 @@
+fruits = ["laranja", "maçã", "uva", "abacaxi"]
+
+fruits[0]
+
+fruits[-1]
+
+fruits.append("banana")
+
+fruits.remove("abacaxi")
+
+fruits.extend(["pera", "melão", "kiwi"])
+
+fruits.index("maçã")
+
+fruits.sort()
