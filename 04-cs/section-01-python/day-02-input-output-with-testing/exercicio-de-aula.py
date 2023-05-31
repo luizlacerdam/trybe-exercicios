be52fdf5d46d9008087eb833708f469035870e5a
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-31 15:18:19 -0300

    ex-02

--- 04-cs/section-01-python/day-02-input-output-with-testing/exercicio-de-aula.py
+++ 04-cs/section-01-python/day-02-input-output-with-testing/exercicio-de-aula.py
@@ -1,6 +1,22 @@
 
 # 01
-name = input('Digite seu nome: ')
+def name_letters(): 
+    name = input('Digite seu nome: ')
 
-for letra in name:
-    print(letra)
+    for letra in name:
+        print(letra)
+
+# 02
+def numbers_sum():
+    numbers = []
+    soma = 0
+    while len(numbers) < 1:
+        numbers = input('Digite os numeros separados por um espaço: ').split()
+    for number in numbers:
+        if not number.isdigit(): 
+            print(f'Erro ao somar valores, {number} é um valor inválido')
+        else:
+            soma += int(number)
+    print(soma)
+
+# 03
