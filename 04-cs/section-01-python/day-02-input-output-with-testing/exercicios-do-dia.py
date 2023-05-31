589385a2e382e577f0aa4b886572531d36125c4e
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-31 19:26:51 -0300

    ex-04

--- 04-cs/section-01-python/day-02-input-output-with-testing/exercicios-do-dia.py
+++ 04-cs/section-01-python/day-02-input-output-with-testing/exercicios-do-dia.py
@@ -19,8 +19,6 @@ def scrambled_words():
         anwser = input('Digite a palavra: ')
         tentativas += 1
     
-
-
 # 03
 def scrambled_words():
     lista = []
@@ -36,5 +34,40 @@ def scrambled_words():
     while anwser != word_chosen and tentativas < 3:
         anwser = input('Digite a palavra: ')
         tentativas += 1
-    
-scrambled_words()
+
+# 04
+import json
+import csv
+
+def calculate_percentage_by_category(book_count_by_category, total_books):
+    return [
+        [category, num_books / total_books]
+        for category, num_books in book_count_by_category.items()
+    ]
+
+def write_csv_report(file, header, rows):
+    writer = csv.writer(file)
+    writer.writerow(header)
+    writer.writerows(rows)
+
+
+def percent_books():
+    categories = {}
+    books = []
+    with open('books.json') as file:
+        books = json.load(file)
+   
+    for book in books:
+        for category in book["categories"]:
+           if not categories.get(category):
+                categories[category] = 0
+        categories[category] += 1
+    number_of_books = len(books)
+    books_percentage_rows = calculate_percentage_by_category(
+        categories, number_of_books
+    )
+    header = ["categoria", "percentagem"]
+    with open("report.csv", "w") as file:
+        write_csv_report(file, header, books_percentage_rows)
+   
+percent_books()
