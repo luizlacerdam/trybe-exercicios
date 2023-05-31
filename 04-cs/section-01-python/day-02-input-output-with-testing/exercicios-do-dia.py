04787886f45d45dda6463ea0e839fdffa90c64af
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-31 18:54:49 -0300

    ex-02

--- 04-cs/section-01-python/day-02-input-output-with-testing/exercicios-do-dia.py
+++ 04-cs/section-01-python/day-02-input-output-with-testing/exercicios-do-dia.py
@@ -5,4 +5,18 @@ def vertical_escada_invertida(nome):
         print(nome)
         nome = nome[:-1]
 
-vertical_escada_invertida('Luiz')
+
+# 02
+import random
+def scrambled_words():
+    lista = ['cuscuz', 'sushi', 'pizza', 'hamburguer', 'churrasco']
+    word_chosen = random.choice(lista)
+    scrambled_word = "".join(random.sample(word_chosen, len(word_chosen)))
+    anwser = ''
+    print(scrambled_word)
+    tentativas = 0
+    while anwser != word_chosen and tentativas < 3:
+        anwser = input('Digite a palavra: ')
+        tentativas += 1
+    
+scrambled_words()
