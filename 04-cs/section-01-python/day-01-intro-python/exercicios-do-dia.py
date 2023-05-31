1bf82b386f0a766cd20b3b5c8f2b20f61e1085bc
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-31 10:23:17 -0300

    ex-02

--- 04-cs/section-01-python/day-01-intro-python/exercicios-do-dia.py
+++ 04-cs/section-01-python/day-01-intro-python/exercicios-do-dia.py
@@ -7,3 +7,15 @@ def maiorDeDois(n1, n2):
         print(f'{n2} é o número maior')
 
 maiorDeDois(3,2)
+
+#02
+
+list = [0,1,2,3,4,5,6,7,8,9]
+
+def mediaAritimetica(arr):
+    total = 0
+    for n in arr:
+        total += n
+    return print(total / len(arr))
+
+mediaAritimetica(list)
