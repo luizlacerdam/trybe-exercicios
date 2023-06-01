45dfb07ae3a97cb52ddccb10cb6c70aad2e19a2b
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-06-01 20:20:04 -0300

    ex-05

--- 04-cs/section-01-python/day-02-input-output-with-testing/exercicios_do_dia.py
+++ 04-cs/section-01-python/day-02-input-output-with-testing/exercicios_do_dia.py
@@ -74,12 +74,12 @@ def percent_books():
 def fizzbuzz(n):
     lista = []
     for i in range(1, n + 1):
-        if i % 3 == 0:
+        if i % 15 == 0:
+            lista.append("FizzBuzz")
+        elif i % 3 == 0:
             lista.append("Fizz")
         elif i % 5 == 0:
             lista.append("Buzz")
-        elif i % 3 == 0 and i % 5 == 0:
-            lista.append("FizzBuzz")
         else:
             lista.append(i)
 
