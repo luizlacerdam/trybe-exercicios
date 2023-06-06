2a64f19a52c3948dab277b360f71fd6373d8e34c
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-06-06 16:52:20 -0300

    ex-01-do-dia

--- 04-cs/section-02-algorithms/day-02-recursividade/exercicio_do_dia.py
+++ 04-cs/section-02-algorithms/day-02-recursividade/exercicio_do_dia.py
@@ -3,12 +3,20 @@
 
 def pair_counter(n):
     counter = 0
-    num = 1
-    while num <= n:
-        if num % 2 == 0:
+    for num in range(n + 1):
+        if num % 2 == 0 and num != 0:
             counter += 1
-        num += 1
     return counter
 
 
-print(pair_counter(3))
+# def pair_counter(n): while loop
+#     counter = 0
+#     num = 1
+#     while num <= n:
+#         if num % 2 == 0:
+#             counter += 1
+#         num += 1
+#     return counter
+
+
+print(pair_counter(4))
