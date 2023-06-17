b4b0518e480d83d8c1f55980e81c4c04c4d8feca
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-06-17 13:39:59 -0300

    :sparkles: [feat] queue-done

--- 04-cs/section-03-data-structs-linea-lists/day-04-queue-stack/queue_1.py
+++ 04-cs/section-03-data-structs-linea-lists/day-04-queue-stack/queue_1.py
@@ -2,6 +2,16 @@ class Queue():
     def __init__(self):
         self.queue = list()
 
+    def __str__(self):
+        str_items = ""
+        for i in range(len(self._data)):
+            value = self._data[i]
+            str_items += str(value)
+            if i + 1 < len(self._data):
+                str_items += ", "
+
+        return "Queue(" + str_items + ")"
+
     def enqueue(self, value):
         self.queue.append(value)
 
