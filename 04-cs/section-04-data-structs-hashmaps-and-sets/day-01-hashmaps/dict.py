410b105f89ffb0b594731824258bd08aab3b47f2
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-06-20 12:13:43 -0300

    ex-5-dict-string_counter

--- 04-cs/section-04-data-structs-hashmaps-and-sets/day-01-hashmaps/dict.py
+++ 04-cs/section-04-data-structs-hashmaps-and-sets/day-01-hashmaps/dict.py
@@ -1,1 +1,12 @@
+# ex 5
 {x: 2 * x for x in range(3, 21)}
+
+
+# {'h': 1, 'e': 1, 'l': 2, 'o': 1}
+# aabbaaccbb
+# {'a': 4, 'b': 4, 'c': 2}
+def string_counter(string):
+    counts = dict.fromkeys(string, 0)
+    for char in string:
+        counts.update({char: counts[char] + 1})
+    return counts
