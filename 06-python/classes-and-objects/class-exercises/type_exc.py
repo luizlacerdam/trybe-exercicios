fbcb726e238534ccf30f055f7e81f1e9ced56117
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-05-19 13:53:13 -0400

    exc-1-and-2

---
+++ 06-python/classes-and-objects/class-exercises/type_exc.py
@@ -0,0 +1,8 @@
+def add_two_numbers(num1: int, num2:int) -> int:
+    return num1 + num2
+
+class Person:
+    def __init__(self: object, name: str, age: int, height: float):
+        self.name = name
+        self.age = age
+        self.height = height
