c11174282d0f26ffd0b24b45e4365a28e4da37f9
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-05-19 14:16:53 -0400

    exc-7-and-8

--- 06-python/classes-and-objects/class-exercises/Database.py
+++ 06-python/classes-and-objects/class-exercises/Database.py
@@ -1,5 +1,33 @@
+from typing import Optional, Union
 class Database:
-    pass
+    def __init__(
+            self,
+            dialect: str, 
+            user: str, 
+            host: str, 
+            password: str, 
+            database: str, 
+            port: Optional[Union[str, int]] = "",
+        ) -> None:
+       
+        if type(port) == str and not port.isnumeric():
+            raise ValueError("`port` must have a numeric value")
+        
+        if not port:
+            if dialect == "mysql":
+                port = 3306
+            elif dialect == "postgres":
+                port = 5432
+            else:
+                raise ValueError(
+                    "invalid default `port` for unrecognized `dialect`"
+                    )
+
+        self.connection_url = (
+            f"{dialect}://{user}:{password}" f"@{host}:{port}/{database}"
+        )
+        
+        
 
 db1 = Database()
 db2 = Database()
