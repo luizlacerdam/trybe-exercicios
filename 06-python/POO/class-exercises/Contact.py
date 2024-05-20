1859695c68b730cef4460913e2ce61cbef5b5f03
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-05-19 22:37:17 -0400

    fix-exc-2

--- 06-python/POO/class-exercises/Contact.py
+++ 06-python/POO/class-exercises/Contact.py
@@ -1,12 +1,13 @@
+from typing import List
 class Contact:
     def __init__(self, name: str, email: str) -> None:
         self.name = name
         self.email = email
 
 class ContactList:
-    def __init__(self, contacts: list, favorites: list) -> None:
-        self.contacts = contacts
-        self.favorites = favorites
+    def __init__(self) -> None:
+        self.contacts = List[Contact] = []
+        self.favorites = List[Contact] = []
 
     def add_contact(self, contact: Contact):
         self.contacts.append(contact)
@@ -17,14 +18,18 @@ class ContactList:
         self.contacts.remove(contact)
 
     def add_to_favorites(self, contact: Contact):
+        if contact in self.favorites:
+            raise LookupError('Contact already in favorites')
         if contact not in self.contacts:
             raise LookupError('Contact not in list')
         self.favorites.append(contact)
+        self.contacts.remove(contact)
 
     def remove_from_favorites(self, contact: Contact):
         if contact not in self.contacts:
             raise LookupError('Contact not in list')
         self.favorites.remove(contact)
+        self.add_contact(contact)
 
 lista = ContactList([], [])
 contact1 = Contact('John', 'john@email.com')
