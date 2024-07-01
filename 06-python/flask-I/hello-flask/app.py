4f0d4256721c0c40d8bbb955646d8f5e35c97d1d
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-07-01 16:19:52 -0400

    hello-world-flask

--- 06-python/flask-I/hello-flask/app.py
+++ 06-python/flask-I/hello-flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+
+@app.route('/api')
+def api():
+    return jsonify({'message': 'Hello, World!'})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
